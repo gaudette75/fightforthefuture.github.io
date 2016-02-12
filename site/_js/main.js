de66bf8672eb8a8e402ec184d2b5564fbdee918e
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-02-11 21:11:18 -0600

    refactored homepage

--- site/_js/main.js
+++ site/_js/main.js
@@ -1,21 +1,64 @@
 (function (doc, win) {
   "use strict";
 
-  win.components = win.components || {};
+  if (document.body.className.indexOf('homepage') !== -1) {
+    (function() {
 
-  var
-    i = 0,
-    components = doc.getElementsByTagName('body')[0].dataset.components;
+      var headerElement = document.querySelector('header');
+      var zoomElement = document.querySelector('.zoom-out-intro');
 
-  if (components !== undefined) {
-    components = components.split(' ');
-    i = components.length;
-  }
+      var oldOpacity = 1;
 
-  while (i--) {
-    if (components[i] !== '' && win.components[components[i]] !== undefined) {
-      win.components[components[i]](doc, win);
-    }
+      var onScroll = function() {
+        var newOpacity = Math.max(1 - (window.pageYOffset / 320), 0);
+
+        if (newOpacity === oldOpacity)
+          return;
+
+        var newScale = (5 / 6) + (newOpacity / 6);
+
+        headerElement.style.opacity = newOpacity;
+        zoomElement.style.opacity = newOpacity;
+        zoomElement.style.transition = 'none';
+        zoomElement.style.transform = 'scale(' + newScale + ')';
+        zoomElement.style.webkitTransform = 'scale(' + newScale + ')';
+
+        oldOpacity = newOpacity;
+      }
+
+      window.addEventListener('scroll', onScroll, false);
+    })();
+
+
+
+    // Preloading & fading in David background
+    (function() {
+      var url = 'homepage/images/david-by-michelangelo.jpg';
+      var image = new Image();
+      image.src = url;
+      image.onload = function() {
+        document.querySelector('.david-by-michelangelo').className += ' loaded ';
+      }
+    })();
+
+    // Mobile navigation
+    (function() {
+      var cheeseburger = document.querySelector('.cheeseburger');
+      var navElement = document.querySelector('nav.top');
+      var mobileNavElement = document.querySelector('.mobile-nav');
+      var mobileNavigationIsExpanded = false;
+
+      cheeseburger.addEventListener('click', function(e) {
+        if (mobileNavigationIsExpanded) {
+          navElement.className = navElement.className.replace(' expanded ', '');
+          mobileNavElement.className = mobileNavElement.className.replace(' expanded ', '');
+        } else {
+          navElement.className += ' expanded ';
+          mobileNavElement.className += ' expanded ';
+        }
+        mobileNavigationIsExpanded = !mobileNavigationIsExpanded;
+      }, false);
+    })();
   }
 
 })(document, window);
